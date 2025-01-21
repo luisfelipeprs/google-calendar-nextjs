f97eb9779f250c4133d85824f00198b13a118885
luisfelipeprs <feliperaxz@gmail.com>
2025-01-21 17:58:21 -0300

    refactor(Schedule): simplify state management and enhance UI components

--- src/pages/Schedule/index.tsx
+++ src/pages/Schedule/index.tsx
@@ -1,46 +1,32 @@
-import { useState, useEffect } from 'react';
-import Link from 'next/link';
+import React, { useState } from 'react';
+import CalendarComponent from '../Components/CalendarComponent';
+import TimeSelectComponent from '../Components/TimeSelectComponent';
+import ButtonComponent from '../Components/ButtonComponent';
 
-const Schedule = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    selectedDateTime: '',
+const SchedulePage: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState<Date>(() => {
+    const today = new Date();
+    today.setDate(today.getDate() + 1); // Adiciona um dia
+    return today;
   });
-  type AvailableTime = {
-    start: string;
-    end: string;
-  };
-  
-  const [availableTimes, setAvailableTimes] = useState<AvailableTime[]>([]);
-  
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
+  const [selectedTime, setSelectedTime] = useState<string>('');
+  const [name, setName] = useState<string>('your-name-here');
+  const [email, setEmail] = useState<string>('your-email-here');
+
+  const handleDateChange = (date: Date) => {
+    setSelectedDate(date);
   };
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const handleScheduleClick = async () => {
+    console.log(name, email, selectedTime);
 
-    if (!formData.name || !formData.email || !formData.selectedDateTime) {
-      alert('Por favor, preencha todos os campos.');
+    if (!name || !email || !selectedTime) {
+      alert('Por favor, preencha todos os campos e selecione um horário.');
       return;
     }
 
-    const selectedDateTime = new Date(Number(formData.selectedDateTime));
-    console.log(formData.name, formData.email, selectedDateTime);
-
-    if (isNaN(selectedDateTime.getTime())) {
-      alert('Data e hora inválidas.');
-      return;
-    }
-
-    const date = selectedDateTime.toISOString().split('T')[0];
-    const time = selectedDateTime.toISOString().split('T')[1].split('.')[0];
+    const dateString = selectedDate.toISOString().split('T')[0];
 
     try {
       const response = await fetch('/api/google/create-event', {
@@ -49,110 +35,42 @@ const Schedule = () => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          name: formData.name,
-          email: formData.email,
-          date,
-          time,
+          name,
+          email,
+          date: dateString,
+          time: selectedTime,
         }),
       });
 
-      if (response.ok) {
-        alert('Reunião marcada com sucesso!');
-        setFormData({ name: '', email: '', selectedDateTime: '' });
-        fetchAvailableTimes();
-      } else {
-        alert('Erro ao marcar a reunião.');
+      if (!response.ok) {
+        throw new Error('Erro ao agendar a reunião.');
       }
-    } catch (error) {
-      console.error(error);
-      alert('Erro ao marcar a reunião.');
-    }
-  };
 
-  const fetchAvailableTimes = async () => {
-    try {
-      const response = await fetch('/api/google/available-times', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          startDate: new Date().toISOString(),
-          endDate: new Date(new Date().setDate(new Date().getDate() + 7)).toISOString(),
-        }),
-      });
       const data = await response.json();
-      setAvailableTimes(data);
+      alert('Reunião marcada com sucesso!');
     } catch (error) {
-      console.error('Erro ao buscar horários disponíveis', error);
+      console.error('Erro ao marcar a reunião:', error);
+      alert('Houve um erro ao marcar a reunião.');
     }
   };
-
-  useEffect(() => {
-    fetchAvailableTimes();
-  }, []);
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-indigo-500 to-purple-700">
-      <div className="bg-white p-8 rounded-lg shadow-xl w-full max-w-lg space-y-8">
-      <Link href="/">
-          <p className="text-indigo-600 hover:text-indigo-800 font-semibold underline">
-            &larr; Voltar para Home
-          </p>
-        </Link>
-        <h1 className="text-3xl font-bold text-center text-gray-800 mb-4">Agendar Reunião</h1>
-        <form onSubmit={handleSubmit} className="space-y-6">
-          <div>
-            <label className="block text-sm font-medium text-gray-600">Nome</label>
-            <input
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              required
-              className="w-full px-4 py-3 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              placeholder="Seu nome"
-            />
-          </div>
-          <div>
-            <label className="block text-sm font-medium text-gray-600">E-mail</label>
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-              className="w-full px-4 py-3 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              placeholder="seuemail@exemplo.com"
-            />
-          </div>
-          <div>
-            <label className="block text-sm font-medium text-gray-600">Escolha o horário</label>
-            <select
-              name="selectedDateTime"
-              value={formData.selectedDateTime}
-              onChange={handleChange}
-              className="w-full px-4 py-3 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
-            >
-              <option value="">Selecione um horário</option>
-              {availableTimes.map((time, index) => (
-                <option key={index} value={time.start}>
-                  {new Date(time.start).toLocaleString()} - {new Date(time.end).toLocaleString()}
-                </option>
-              ))}
-            </select>
-          </div>
-
-          <button
-            type="submit"
-            className="w-full bg-indigo-600 text-white py-3 rounded-lg hover:bg-indigo-700 transition-all"
-          >
-            Agendar
-          </button>
-        </form>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-xl w-full max-w-2xl flex">
+        <div className="flex-1">
+          <CalendarComponent selectedDate={selectedDate} onDateChange={handleDateChange} />
+        </div>
+        <div className="w-[2px] bg-gray-300 mx-10"></div>
+        <div className="flex-1 flex flex-col justify-center items-center space-y-4">
+          <TimeSelectComponent
+            selectedDate={selectedDate}
+            onTimeChange={(time) => setSelectedTime(time)}
+          />
+          <ButtonComponent onClick={handleScheduleClick} text="Agendar consultoria gratuita" />
+        </div>
       </div>
     </div>
   );
 };
 
-export default Schedule;
+export default SchedulePage;
